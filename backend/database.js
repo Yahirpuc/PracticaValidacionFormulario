fc994d709127bfef2f5f8a4c15f6ef7175080ff5
Yahir Ordoñez <41529436+Yahirpuc@users.noreply.github.com>
2026-02-20 22:07:53 -0600

    Actualizar database.js

--- backend/database.js
+++ backend/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-// Ubicación del archivo de base de datos
+
 const dbPath = path.resolve(__dirname, 'ciber_ciudadanos.db');
 
 const db = new sqlite3.Database(dbPath, (err) => {
@@ -9,7 +9,7 @@ const db = new sqlite3.Database(dbPath, (err) => {
     else console.log("Conectado a SQLite: ciber_ciudadanos.db");
 });
 
-// Definición de la tabla con restricciones de unicidad 
+
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
